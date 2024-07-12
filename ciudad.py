70cbd5297bdc53ff864d143bbe0d63ab8de6127c
alvaroMartinez13 <alvaro.martinezalcina13@gmail.com>
2024-07-12 16:12:47 -0500

    refactor: :triangular_flag_on_post: Función Ver ciudadades
    Ahora se encuentra en funcionamiento la función ver_ciudades
    
    By @Alvaro M.

---
+++ ciudad.py
@@ -0,0 +1,29 @@
+import config
+
+ciudad = "ciudad.json"
+
+datos_ciudad = config.cargar_datos(ciudad)
+
+#Si existe ciudad, no se deja crear
+def existe_ciudad(clave, valorNuevo):
+    for ciudad_existente in datos_ciudad["ciudad"]:
+        if ciudad_existente[clave] == valorNuevo:
+            print("Valor existe")
+            return False
+    return True
+
+#Agrega una ciudad
+def nueva_ciudad(nuevaCiudad):
+    print()
+
+def ver_ciudades():
+    for ciudad_existente in datos_ciudad["ciudad"]:
+        print(f"Ciudad: {ciudad_existente['nombre']} Código postal: {ciudad_existente['codigo_postal']} Poblacion estimada: {ciudad_existente['poblacion_estimada']} Pais: {ciudad_existente['pais']}")
+
+def edita_ciudad(clave, valorAntiguo, valorNuevo):
+    print()
+    
+
+
+
+
